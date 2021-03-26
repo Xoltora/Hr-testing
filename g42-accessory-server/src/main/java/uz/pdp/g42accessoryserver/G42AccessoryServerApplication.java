0a470f3b95fb12d151f84526fcf5dc98ed6e84e8
@alisherabuhabiba <alisherabuhabiba@gmail.com>
2021-03-26 19:28:26 +0500

    adding server side

---
+++ g42-accessory-server/src/main/java/uz/pdp/g42accessoryserver/G42AccessoryServerApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.g42accessoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G42AccessoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G42AccessoryServerApplication.class, args);
+    }
+
+}
---
+++ g42-accessory-server/src/test/java/uz/pdp/g42accessoryserver/G42AccessoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.g42accessoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G42AccessoryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
