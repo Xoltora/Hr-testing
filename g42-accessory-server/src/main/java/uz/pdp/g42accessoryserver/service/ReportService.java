7af65ee30acaff4aef67cadfa18cf1a9e0ef911d
Jamshid Ismatov <jamshidismatov1998@gmail.com>
2021-04-06 01:19:34 +0500

    6.4.2021 1:19

---
+++ g42-accessory-server/src/main/java/uz/pdp/g42accessoryserver/service/ReportService.java
@@ -0,0 +1,39 @@
+package uz.pdp.g42accessoryserver.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import uz.pdp.g42accessoryserver.entity.Report;
+import uz.pdp.g42accessoryserver.entity.User;
+import uz.pdp.g42accessoryserver.payload.ApiResponse;
+import uz.pdp.g42accessoryserver.repository.ReportRepository;
+import uz.pdp.g42accessoryserver.repository.UserRepository;
+import uz.pdp.g42accessoryserver.utills.CommonUtills;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    ReportRepository reportRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+//    public ApiResponse all(Integer page, Integer size) throws IllegalAccessException {
+//        Page<User> all = userRepository.findAll(CommonUtills.getPageableByCreatedAtDesc(page, size));
+//        return new ApiResponse("Ok",true,all.getContent().stream().map(this::getUserDto).collect(Collectors.toList()),all.getTotalElements(),all.getTotalPages());
+//    }
+
+    public ApiResponse getReport(boolean accepted) {
+        List<Report> reports = reportRepository.findByAcceptedEquals(accepted);
+        return new ApiResponse("Success", true, reports);
+    }
+
+    public ApiResponse updateReport(boolean accepted) {
+        reportRepository.changeStatus();
+        return new ApiResponse("Success", true);
+    }
+}
